a93b006fac0c2d84d27f1d969afe43e9c4fae5ef
Xiaofan2 <xiaofan2.rs@icloud.com>
2020-02-24 19:20:05 +0800

    Initial Commit

---
+++ Lifetool/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lifetool
+//
+//  Created by 范艺晨 on 2020/2/24.
+//  Copyright © 2020 SUESiOSClub. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
